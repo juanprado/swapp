f4096817bb0f095c6a36cf965136fe4a6e519b71
Juan Prado <juanprado.ux@gmail.com>
2019-05-12 13:17:47 -0400

    creating a user with a valid username

--- src/views/pages/createUser.js
+++ src/views/pages/createUser.js
@@ -1,26 +1,34 @@
 import React from 'react';
+
 import firebase from '../../config/firebase';
 import UserForm from '../components/userForm';
 
-const createUser = async (user) => {
-  const { firstName, lastName, email } = user;
-  const id = email.split('@')[0];
+const CreateUser = (props) => {
+  console.log(props)
+  const { history } = props;
 
-  const db = firebase.firestore();
+  const createUser = async (newUser) => {
+    const { firstName, lastName, email } = newUser;
+    const id = email.split('@')[0];
+    const db = firebase.firestore();
+    let user;
 
-  try {
-    const userRef = await db.collection('users').add({
-      id,
-      name: { first: firstName, last: lastName },
-      email
-    });
+    try {
+      await db.collection('users').doc(id).set({
+        name: { first: firstName, last: lastName },
+        email
+      });
 
-    alert(`user ${userRef.id} created :)`);
-  } catch(error) {
-    console.log(error);
-  }
+      user = await db.collection('users').doc(id);
+      history.push(`/dashboard/${id}`);
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <UserForm onSubmit={createUser} />
+  );
 };
-
-const CreateUser = () => <UserForm onSubmit={createUser} />;
 
 export default CreateUser;
